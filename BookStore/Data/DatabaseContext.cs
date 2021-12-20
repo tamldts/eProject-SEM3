7c7f0b4f0a75b74b5e5023db508cbc13d075df98
tamldts <96402668+tamldts@users.noreply.github.com>
2021-12-20 10:57:33 +0700

    Add files via upload

---
+++ BookStore/Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options) { }
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Category> Categorys { get; set; }
+    }
+}
---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    [Table("tbBook")]
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BookID { get; set; }
+        public string BookTitle { get; set; }
+        public int BookQuantity { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal BookPrice { get; set; }
+    }
+}
